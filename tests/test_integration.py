378aa5432ed11e1a051fb148a1ff80d49b773d9c
Thomas Rausch <thomas@thrau.at>
2023-04-02 15:41:29 +0200

    add test code to wait for lambdas

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -4,11 +4,13 @@ import typing
 import uuid
 
 import boto3
+import pytest
 import requests
 
 if typing.TYPE_CHECKING:
     from mypy_boto3_s3 import S3Client
     from mypy_boto3_ssm import SSMClient
+    from mypy_boto3_lambda import LambdaClient
 
 os.environ["AWS_DEFAULT_REGION"] = "us-east-1"
 os.environ["AWS_ACCESS_KEY_ID"] = "test"
@@ -20,6 +22,17 @@ s3: "S3Client" = boto3.client(
 ssm: "SSMClient" = boto3.client(
     "ssm", endpoint_url="http://localhost.localstack.cloud:4566"
 )
+awslambda: "LambdaClient" = boto3.client(
+    "lambda", endpoint_url="http://localhost.localstack.cloud:4566"
+)
+
+
+@pytest.fixture(autouse=True)
+def _wait_for_lambdas():
+    # makes sure that the lambdas are available before running integration tests
+    awslambda.get_waiter("function_active").wait(FunctionName="presign")
+    awslambda.get_waiter("function_active").wait(FunctionName="resize")
+    awslambda.get_waiter("function_active").wait(FunctionName="list")
 
 
 def test_s3_resize_integration():
