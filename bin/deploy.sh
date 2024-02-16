d64b28c2fba762a42fa864796684c65f8185738d
lakkeger <gabor.jozsef.nemeth@localstack.cloud>
2024-02-16 13:15:15 +0100

    fix(ci:gitlab): lambda deps

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -61,7 +61,7 @@ else
         cd lambdas/resize
         rm -rf package lambda.zip
         mkdir package
-        pip install -r requirements.txt -t package
+        pip install -r requirements.txt --platform manylinux2014_x86_64 --only-binary=:all: -t package
         zip lambda.zip handler.py
         cd package
         zip -r ../lambda.zip *;
